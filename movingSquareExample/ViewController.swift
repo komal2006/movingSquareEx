4683bcd809bd67f222bc732902b8552dfd010596
Komal2006 <komaldeepkr99@gmail.com>
2020-01-06 21:21:28 -0500

    moving square

--- movingSquareExample/ViewController.swift
+++ movingSquareExample/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var squareImg: UIImageView!
+    
+     var location = CGPoint(x: 0,y: 0)
+     
+     func moveSquare(_ imageView: UIImageView,_ speed:CGFloat)
+     {
+         let speeds = speed
+         let imageSpeed = speeds / view.frame.size.height
+         let averageSpeed = (view.frame.size.height - squareImg.frame.origin.x) * imageSpeed
+         UIView.animate(withDuration: TimeInterval(averageSpeed), delay: 0.0, options: .curveLinear, animations: {
+         imageView.frame.origin.y = self.view.frame.size.height
+       }, completion: { (_) in
+       imageView.frame.origin.y = -imageView.frame.size.height
+       self.moveSquare(imageView,speeds)
+       })
     }
 
-
+    override func viewDidAppear(_ animated: Bool)
+    {
+        moveSquare(squareImg, 30)
+    }
 }
 
